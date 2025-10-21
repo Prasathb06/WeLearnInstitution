ba7d523d4cff42957eaa6cab40da59eb1328079f
Prasathb06 <arunprasathb045@gmail.com>
2025-10-21 20:15:05 +0530

    Fix: use node instead of nodemon in start script

--- project_r_nd/Controller/UserController.js
+++ project_r_nd/Controller/UserController.js
@@ -1,5 +1,5 @@
 const { body, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const UserModel = require('../Models/UserModels');
 
 exports.register = [
