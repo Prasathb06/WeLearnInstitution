dcb50974b94da86415de254a23807b0373d09a31
Prasathb06 <arunprasathb045@gmail.com>
2025-10-28 10:13:29 +0530

    Initial commit

--- project_r/src/Component/CnavBar.jsx
+++ project_r/src/Component/CnavBar.jsx
@@ -7,22 +7,22 @@ const CnavBar = () => {
     <div>
       <Nav fill variant="tabs" defaultActiveKey="/home">
         <Nav.Item>
-          <Nav.Link eventKey="" href='HC'>HTML & CSS</Nav.Link>
+          <Nav.Link eventKey=""><Link to="/javascript" className='nav-link'>HTML & CSS</Link></Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2" href='javascript'>JavaScript</Nav.Link>
+          <Nav.Link eventKey="link-2">JavaScript<Link to="/javascript" className='nav-link'>JavaScript</Link></Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3" href='java'>Java</Nav.Link>
+          <Nav.Link eventKey="link-3"><Link to="/java" className='nav-link'>Java</Link></Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-4">NodeJS</Nav.Link>
+          <Nav.Link eventKey="link-4"><Link to="/javascript" className='nav-link'>NodeJS</Link></Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-5">ReactJS</Nav.Link>
+          <Nav.Link eventKey="link-5"><Link to="/javascript" className='nav-link'>ReactJS</Link></Nav.Link>
         </Nav.Item>
         {/* <Nav.Item>
-          <Nav.Link eventKey="link-6">Disabled</Nav.Link>
+          <Nav.Link eventKey="link-6">Disabled<Link to="/javascript" className='nav-link'>JavaScript</Link></Nav.Link>
         </Nav.Item> */}
       </Nav>
     </div>
